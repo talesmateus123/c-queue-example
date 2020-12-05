b5457c511c22001240a215bf4457ca097bc02496
talesmateus123 <talesmateus1999@hotmail.com>
2020-12-05 16:10:53 -0400

    Implemented showQueue method.

--- main.c
+++ main.c
@@ -11,35 +11,49 @@ typedef struct c {
 	node *end;
 } queue;
 
+void printLine();
 void startQueue(queue *q);
-void insert(queue *q, int number);
+void insertQueue(queue *q, int number);
+void showQueue(queue *q);
 
 int main(int argc, char const *argv[]) {
 	queue *q = malloc(sizeof(queue));
 	startQueue(q);
 
-	insert(q, 1);
-	printf("%d %d\n", q->start->number, q->end->number);
-
-	insert(q, 2);
-	printf("%d %d\n", q->start->number, q->end->number);
+	insertQueue(q, 1);
+	showQueue(q);
+	insertQueue(q, 2);
+	showQueue(q);
 	return 0;
 }
 
+void printLine() {
+	printf("----------------------------\n");
+}
 void startQueue(queue *q) {
-	q->start=NULL;
-	q->end=NULL;
+	q->start = NULL;
+	q->end = NULL;
 }
 
-void insert(queue *q, int number) {
+void insertQueue(queue *q, int number) {
 	node *new = malloc(sizeof(node));
-	new->number=number;
-	new->next=NULL;
+	new->number = number;
+	new->next = NULL;
 	if(q->end != NULL) {
-		q->end->next=new;
+		q->end->next = new;
 	}
 	else {
-		q->start=new;
+		q->start = new;
 	}
 	q->end=new;
+}
+
+void showQueue(queue *q) {
+	node *aux;
+	aux = q->start;
+	while(aux != NULL) {
+		printf("%d\n", aux->number);
+		aux=aux->next;
+	}
+	printLine();
 }
